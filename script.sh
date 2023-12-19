37114ce1b31b04d877b532fb6d70ef711e75bd62
concr <k@wchrt.de>
2023-12-19 21:22:57 +0100

    AI changes

--- script.sh
+++ script.sh
@@ -11,30 +11,39 @@ function echo_three { echo "three"; }
 function echo_four { echo "four"; }
 function echo_five { echo "five"; }
 
-STAGE_FUNCTIONS=("echo_one" "echo_two" "echo_three" "echo_four" "echo_five")
+STAGE_FUNCTIONS=(
+    "echo_one"
+    "echo_two"
+    "echo_three"
+    "echo_four"
+    "echo_five"
+)
+
 STAGES_OK=0
 
-echo "Stages will be executed in the following order:"
-echo ""
+# Display the order and names of the functions
 
+echo "Stages will be executed in the following order:"
+echo
 for STAGE in $(echo $STAGES | grep -o .); do
     STAGE_FUNCTION=${STAGE_FUNCTIONS[$STAGE-1]}
-
     echo "Stage: $STAGE - Function: $STAGE_FUNCTION"
 done
 
-echo ""
-read -p "Confirm execution (yes): " CONFIRMATION
-echo ""
+# Confirm execution
 
+echo
+read -p "Confirm execution (yes): " CONFIRMATION
 [[ $CONFIRMATION == "yes" ]] || { echo "Execution aborted."; exit 1; }
 
 # Execute the stages
+
+echo
 for STAGE in $(echo $STAGES | grep -o .); do
     STAGE_FUNCTION=${STAGE_FUNCTIONS[$STAGE-1]}
-
     $STAGE_FUNCTION && ((STAGES_OK++))
 done
 
-echo ""
+# Display the result of the execution
+echo
 [[ $STAGES_OK -eq ${#STAGES} ]] && echo "All stages executed successfully." || echo "Some stages failed."
