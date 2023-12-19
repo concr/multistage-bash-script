718400671585f202a89aa5411b57198187cb8e18
concr <k@wchrt.de>
2023-12-19 20:46:54 +0100

    AI fixes

--- script.sh
+++ script.sh
@@ -2,15 +2,17 @@
 
 [[ $DEBUG ]] && set -x
 
-###
-### vars
-###
+### Variables
 
-STAGES=$1
-[[ $STAGES == "all" ]] && { STAGES="12345"; }
-[[ $STAGES =~ ^[1-5]+$ ]] || { echo "ERROR: no or wrong selected stages ($STAGES)"; exit 1; }
+SELECTED_STAGES=$1
 
-STAGE_FUNCTIONS=(
+case "$SELECTED_STAGES" in
+    "all") STAGES="12345" ;;
+    *[!1-5]* | "") echo "ERROR: Invalid stage selection ('$SELECTED_STAGES'). Please select from stages 1-5."; exit 1 ;;
+    *) STAGES=$SELECTED_STAGES ;;
+esac
+
+STAGE_ACTION_FUNCTIONS=(
     "echo_one"
     "echo_two"
     "echo_three"
@@ -21,70 +23,66 @@ STAGE_FUNCTIONS=(
 STAGES_OK=0
 STAGES_EXPECTED=${#STAGES}
 
-###
-### functions
-###
+### Functions
 
-function loop_stages {
-    local CMD="$1"
-
+function echo_stages {
     for (( i=0; i<${#STAGES}; i++ )); do
         STAGE=${STAGES:$i:1}
-        STAGE_FUNCTION=${STAGE_FUNCTIONS[$STAGE-1]}
+        STAGE_FUNCTION=${STAGE_ACTION_FUNCTIONS[$STAGE-1]}
+        echo "Order: $i - Stage: $STAGE - Function: $STAGE_FUNCTION"
+    done
+}
 
-        case "$CMD" in
-            echo_stages)
-                echo "Order: $i - Stage: $STAGE - Function: $STAGE_FUNCTION";;
-            exec_stages)
-                eval $STAGE_FUNCTION;;
-        esac
+function exec_stages {
+    for (( i=0; i<${#STAGES}; i++ )); do
+        STAGE=${STAGES:$i:1}
+        STAGE_FUNCTION=${STAGE_ACTION_FUNCTIONS[$STAGE-1]}
+        eval "$STAGE_FUNCTION"
     done
 }
 
 function echo_one {
-    echo "one" && \
+    echo "one"
     ((STAGES_OK++))
 }
 
 function echo_two {
-    echo "two" && \
+    echo "two"
     ((STAGES_OK++))
 }
 
 function echo_three {
-    echo "three" && \
+    echo "three"
     ((STAGES_OK++))
 }
 
 function echo_four {
-    echo "four" && \
+    echo "four"
     ((STAGES_OK++))
 }
 
 function echo_five {
-    echo "five" #&& \
-    #((STAGES_OK++))
+    echo "five"
+    ((STAGES_OK++))
 }
 
-###
-### structured
-###
+### Main Execution
 
-# aks if selection is correct
+# Ask if the selection is correct
 echo "Following stages will run:"
 echo ""
 
-loop_stages echo_stage
+echo_stages
 
 echo ""
 read -p "Please confirm (yes): " YES
 echo ""
 
-[[ $YES ]] || { echo "Aborted. Please confirm with yes to run selected stages."; exit 1; }
+[[ $YES == "yes" ]] || { echo "Aborted. Please confirm with 'yes' to run selected stages."; exit 1; }
 
-loop_stages exec_stage
+exec_stages
 
 echo ""
 [ $STAGES_OK -eq $STAGES_EXPECTED ] \
-    && { echo "All $STAGES_OK of $STAGES_EXPECTED stages went ok"; exit 0; } \
+    && { echo "All $STAGES_OK of $STAGES_EXPECTED stages went ok"; exit 0; }
     || { echo "Only $STAGES_OK of $STAGES_EXPECTED stages went ok"; exit 1; }
