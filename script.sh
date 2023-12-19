b77cf23287adb11164cc692d7dc28105963c71cd
concr <k@wchrt.de>
2023-12-19 20:53:26 +0100

    AI shortened

--- script.sh
+++ script.sh
@@ -25,19 +25,14 @@ STAGES_EXPECTED=${#STAGES}
 
 ### Functions
 
-function echo_stages {
+function handle_stages {
+    local CMD="$1"
     for (( i=0; i<${#STAGES}; i++ )); do
         STAGE=${STAGES:$i:1}
         STAGE_FUNCTION=${STAGE_ACTION_FUNCTIONS[$STAGE-1]}
-        echo "Order: $i - Stage: $STAGE - Function: $STAGE_FUNCTION"
-    done
-}
 
-function exec_stages {
-    for (( i=0; i<${#STAGES}; i++ )); do
-        STAGE=${STAGES:$i:1}
-        STAGE_FUNCTION=${STAGE_ACTION_FUNCTIONS[$STAGE-1]}
-        eval "$STAGE_FUNCTION"
+        [[ "$CMD" == "echo" ]] && echo "Order: $i - Stage: $STAGE - Function: $STAGE_FUNCTION"
+        [[ "$CMD" == "exec" ]] && eval "$STAGE_FUNCTION"
     done
 }
 
@@ -72,7 +67,7 @@ function echo_five {
 echo "Following stages will run:"
 echo ""
 
-echo_stages
+handle_stages "echo"
 
 echo ""
 read -p "Please confirm (yes): " YES
@@ -80,9 +75,13 @@ echo ""
 
 [[ $YES == "yes" ]] || { echo "Aborted. Please confirm with 'yes' to run selected stages."; exit 1; }
 
-exec_stages
+handle_stages "exec"
 
 echo ""
-[ $STAGES_OK -eq $STAGES_EXPECTED ] \
-    && { echo "All $STAGES_OK of $STAGES_EXPECTED stages went ok"; exit 0; }
-    || { echo "Only $STAGES_OK of $STAGES_EXPECTED stages went ok"; exit 1; }
+if [ $STAGES_OK -eq $STAGES_EXPECTED ]; then
+    echo "All $STAGES_OK of $STAGES_EXPECTED stages went ok"
+    exit 0
+else
+    echo "Only $STAGES_OK of $STAGES_EXPECTED stages went ok"
+    exit 1
+fi
