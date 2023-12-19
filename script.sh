6692343e6fb35a35906cd64e0f78844e7080d218
concr <k@wchrt.de>
2023-12-19 21:07:41 +0100

    AI refactor

--- script.sh
+++ script.sh
@@ -2,86 +2,39 @@
 
 [[ $DEBUG ]] && set -x
 
-### Variables
+STAGES=${1:-"12345"}
+[[ $STAGES =~ ^[1-5]+$ ]] || { echo "ERROR: Invalid stage selection ('$STAGES')."; exit 1; }
 
-SELECTED_STAGES=$1
+function echo_one { echo "one"; }
+function echo_two { echo "two"; }
+function echo_three { echo "three"; }
+function echo_four { echo "four"; }
+function echo_five { echo "five"; }
 
-case "$SELECTED_STAGES" in
-    "all") STAGES="12345" ;;
-    *[!1-5]* | "") echo "ERROR: Invalid stage selection ('$SELECTED_STAGES'). Please select from stages 1-5."; exit 1 ;;
-    *) STAGES=$SELECTED_STAGES ;;
-esac
-
-STAGE_ACTION_FUNCTIONS=(
-    "echo_one"
-    "echo_two"
-    "echo_three"
-    "echo_four"
-    "echo_five"
-)
-
+STAGE_FUNCTIONS=("echo_one" "echo_two" "echo_three" "echo_four" "echo_five")
 STAGES_OK=0
-STAGES_EXPECTED=${#STAGES}
 
-### Functions
-
-function handle_stages {
-    local CMD="$1"
-    for (( i=0; i<${#STAGES}; i++ )); do
-        STAGE=${STAGES:$i:1}
-        STAGE_FUNCTION=${STAGE_ACTION_FUNCTIONS[$STAGE-1]}
-
-        [[ "$CMD" == "echo" ]] && echo "Order: $i - Stage: $STAGE - Function: $STAGE_FUNCTION"
-        [[ "$CMD" == "exec" ]] && eval "$STAGE_FUNCTION"
-    done
-}
-
-function echo_one {
-    echo "one"
-    ((STAGES_OK++))
-}
-
-function echo_two {
-    echo "two"
-    ((STAGES_OK++))
-}
-
-function echo_three {
-    echo "three"
-    ((STAGES_OK++))
-}
-
-function echo_four {
-    echo "four"
-    ((STAGES_OK++))
-}
-
-function echo_five {
-    echo "five"
-    ((STAGES_OK++))
-}
-
-### Main Execution
-
-# Ask if the selection is correct
-echo "Following stages will run:"
+echo "Stages will be executed in the following order:"
 echo ""
 
-handle_stages "echo"
+for STAGE in $(echo $STAGES | grep -o .); do
+    STAGE_FUNCTION=${STAGE_FUNCTIONS[$STAGE-1]}
+
+    echo "Stage: $STAGE - Function: $STAGE_FUNCTION"
+done
 
 echo ""
-read -p "Please confirm (yes): " YES
+read -p "Confirm execution (yes): " CONFIRMATION
 echo ""
 
-[[ $YES == "yes" ]] || { echo "Aborted. Please confirm with 'yes' to run selected stages."; exit 1; }
+[[ $CONFIRMATION == "yes" ]] || { echo "Execution aborted."; exit 1; }
 
-handle_stages "exec"
+# Execute the stages
+for STAGE in $(echo $STAGES | grep -o .); do
+    STAGE_FUNCTION=${STAGE_FUNCTIONS[$STAGE-1]}
+
+    $STAGE_FUNCTION && ((STAGES_OK++))
+done
 
 echo ""
-if [ $STAGES_OK -eq $STAGES_EXPECTED ]; then
-    echo "All $STAGES_OK of $STAGES_EXPECTED stages went ok"
-    exit 0
-else
-    echo "Only $STAGES_OK of $STAGES_EXPECTED stages went ok"
-    exit 1
-fi
+[[ $STAGES_OK -eq ${#STAGES} ]] && echo "All stages executed successfully." || echo "Some stages failed."
